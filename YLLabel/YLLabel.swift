b88499b3502a30d8e70418dd4b22e5b1f9fdc89d
张银龙 <469891897@qq.com>
2017-02-18 12:03:42 +0800

    准备开动

---
+++ YLLabel/YLLabel.swift
@@ -0,0 +1,15 @@
+//
+//  YLLabel.swift
+//  YLLabelDemo
+//
+//  Created by 张银龙 on 2017/2/18.
+//  Copyright © 2017年 张银龙. All rights reserved.
+//
+
+import UIKit
+
+class YLLabel: UILabel {
+
+    
+
+}
